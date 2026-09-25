58955d169d6ff9070af471d19df4b3596b2db10d
Shamita Kulkarni <viratshamita@gmail.com>
2026-09-26 02:01:10 +0530

    Fix duplicate declaration of API constant

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API = "const API = "https://paimana-backend-cfv0.onrender.com/api";
+const API ="https://paimana-backend-cfv0.onrender.com/api";
 
 
 // =====================================================
